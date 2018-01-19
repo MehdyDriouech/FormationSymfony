e3ed44ebee11157fef954327d5feef8f23faae73
Mehdy Driouech <mehdy.driouech@oxo-team.com>
2018-01-19 10:21:47 +0100

    Starting blog

---
+++ src/AppBundle/Controller/BlogController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class BlogController extends Controller
+{
+    /**
+     * @Route("/blog", name="BlogPage")
+     */
+    public function blogAction(Request $request)
+    {
+      dump($request);
+        // replace this example code with whatever you need
+        return $this->render('default/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+}
